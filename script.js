253d1433079542a89d04a84266e8bed7e7f37482
chandraprakashmishra593 <chandan589412@gmail.com>
2024-12-06 19:00:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Basic interactivity for the form
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for reaching out! I will get back to you soon.');
+  });
+  
